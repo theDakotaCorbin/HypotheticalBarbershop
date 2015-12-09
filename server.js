3b6e4f12b0579ee9a7a6a95359389c16a5db89c4
theDakotaCorbin <dakotamcorbin@gmail.com>
2015-12-09 15:33:11 -0700

    Updated

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ mongoose.connection.once('open', function() {
 
 
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/Public'));
 
